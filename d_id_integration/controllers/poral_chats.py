0423cd73d01c81e9c671d5a17eaa0fbe4935447c
ezzElzoz123 <ezzeldin@itss-c.com>
2025-12-07 15:45:40 +0200

    add sort and search in list view

--- d_id_integration/controllers/poral_chats.py
+++ d_id_integration/controllers/poral_chats.py
@@ -18,20 +18,36 @@ class PortalChats(CustomerPortal):
         return values
 
     @http.route(['/my/chats', '/my/chats/page/<int:page>'], type='http', website=True)
-    def portal_chats_list_view(self, page=1, **kwargs):
+    def portal_chats_list_view(self, page=1, sortby='id', search="", search_in="All", **kwargs):
+        sorted_list = {
+            'id': {'label': 'Chat Desc', 'order': 'id desc'},
+            'character_name': {'label': 'Character', 'order': 'character'},
+            'question': {'label': 'Question', 'order': 'question'},
+            'lyrics': {'label': 'Answer', 'order': 'lyrics'},
+        }
+        search_list = {
+            'All': {'label': 'All', 'input': 'All', 'domain': []},
+            'character': {'label': 'Character', 'input': 'character', 'domain': self._get_character_domain(search)},
+            'question': {'label': 'Question', 'input': 'question', 'domain': [('question', 'ilike', search)]},
+            'lyrics': {'label': 'Answer', 'input': 'lyrics', 'domain': [('lyrics', 'ilike', search)]},
+        }
+        search_domain = search_list[search_in]['domain']
+        default_order_by = sorted_list[sortby]['order']
         user = request.env.user
-        chat_count = request.env['d.id.api'].search_count([
-            ('create_uid', '=', user.id)
-        ])
-        page_detail = pager(url='/my/chats', total=chat_count, page=page, step=3)
-        chats = request.env['d.id.api'].search([
-            ('create_uid', '=', user.id)
-        ], limit=3, offset=page_detail['offset'])
+        search_domain.append(('create_uid', '=', user.id))
+        chat_count = request.env['d.id.api'].search_count(search_domain)
+        page_detail = pager(url='/my/chats', total=chat_count, page=page, url_args={'sortby': sortby, 'search_in': search_in, 'search': search}, step=3)
+        chats = request.env['d.id.api'].search(search_domain, limit=3, order= default_order_by ,offset=page_detail['offset'])
         values = {
             'page_name': 'chats',
             'chat_count': chat_count,
             'chats': chats,
             'pager': page_detail,
+            'sortby': sortby,
+            'searchbar_sortings': sorted_list,
+            'search_in': search_in,
+            'searchbar_inputs': search_list,
+            'search': search,
         }
         return request.render('d_id_integration.portal_my_chats_list_view', values)
 
@@ -62,3 +78,20 @@ class PortalChats(CustomerPortal):
             values,
             headers={'X-Frame-Options': 'SAMEORIGIN'}
         )
+
+    def _get_character_domain(self, search):
+        if not search:
+            return []
+
+        character_map = {
+            'sara': 'ch1',
+            'abeer': 'ch2',
+        }
+
+        search_lower = search.lower().strip()
+
+        for name, key in character_map.items():
+            if name in search_lower:
+                return [('character', '=', key)]
+
+        return [('character', '=', False)]
