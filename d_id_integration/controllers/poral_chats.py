752d50393730d43feca95b9a3ede724d8915063b
ezzElzoz123 <ezzeldin@itss-c.com>
2025-12-10 17:06:47 +0200

    add group by feature in portal

--- d_id_integration/controllers/poral_chats.py
+++ d_id_integration/controllers/poral_chats.py
@@ -1,8 +1,12 @@
+from itertools import groupby
+
 from PIL.ImageChops import offset
 
 from odoo import http
 from odoo.http import request
 from odoo.addons.portal.controllers.portal import CustomerPortal, pager
+from odoo.tools import groupby as groupbyelem
+from operator import itemgetter
 
 
 class PortalChats(CustomerPortal):
@@ -18,7 +22,9 @@ class PortalChats(CustomerPortal):
         return values
 
     @http.route(['/my/chats', '/my/chats/page/<int:page>'], type='http', auth='public', website=True) # auth -> public for not logged in users, auth -> user for logged in users
-    def portal_chats_list_view(self, page=1, sortby='id', search="", search_in="All", **kwargs):
+    def portal_chats_list_view(self, page=1, sortby='id', search="", search_in="All", groupby="none", **kwargs):
+        if not groupby:
+            groupby = "none"
         sorted_list = {
             'id': {'label': 'Chat Desc', 'order': 'id desc'},
             'character_name': {'label': 'Character', 'order': 'character'},
@@ -31,18 +37,38 @@ class PortalChats(CustomerPortal):
             'question': {'label': 'Question', 'input': 'question', 'domain': [('question', 'ilike', search)]},
             'lyrics': {'label': 'Answer', 'input': 'lyrics', 'domain': [('lyrics', 'ilike', search)]},
         }
+        groupby_list = {
+            'none': {'label': 'None', 'input': 'none', 'order': 1},
+            'character': {'label': 'Character', 'input': 'character', 'order': 1},
+            'question': {'label': 'Question', 'input': 'question', 'order': 1},
+            # 'lyrics': {'label': 'Lyrics', 'input': 'lyrics', 'order': 1},
+        }
+        chat_groupby = groupby_list.get(groupby, {})
         search_domain = search_list[search_in]['domain']
         default_order_by = sorted_list[sortby]['order']
+        if groupby in ["character", "question"]:
+            chat_groupby = chat_groupby.get("input")
+        else:
+            chat_groupby = ''
         user = request.env.user
+        chat_obj = request.env['d.id.api']
         search_domain.append(('create_uid', '=', user.id)) # if I am not logged
         chat_count = request.env['d.id.api'].sudo().search_count(search_domain)
-        page_detail = pager(url='/my/chats', total=chat_count, page=page, url_args={'sortby': sortby, 'search_in': search_in, 'search': search}, step=3)
+        chat_url = '/my/chats'
+        page_detail = pager(url=chat_url, total=chat_count, page=page, url_args={'sortby': sortby, 'search_in': search_in, 'search': search, 'groupby':groupby}, step=3)
         chats = request.env['d.id.api'].sudo().search(search_domain, limit=3, order= default_order_by ,offset=page_detail['offset'])
+        if chat_groupby:
+            chats_group_list = [{'groupby_value': k, 'chats': chat_obj.concat(*g)} for k, g in groupbyelem(chats, itemgetter(chat_groupby))]
+        else:
+            chats_group_list = [{'groupby_value': None, 'chats': chats}]
         values = {
             'page_name': 'chats',
             'chat_count': chat_count,
-            'chats': chats,
+            'group_chats': chats_group_list,
             'pager': page_detail,
+            'default_url': chat_url,
+            'groupby': groupby,
+            'searchbar_groupby': groupby_list,
             'sortby': sortby,
             'searchbar_sortings': sorted_list,
             'search_in': search_in,
